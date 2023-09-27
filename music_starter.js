72b1f5d0f0268e03150fcd4a1e309d28a3f019e9
SamuelDRichards <Sam@samrichardsstudio.com>
2023-09-27 18:12:02 +1300

    made a fish
    added a fish that sings along with the words

--- music_starter.js
+++ music_starter.js
@@ -3,6 +3,9 @@
 
 //background is 1500(width) x 800(height)
 
+let x = -250; //adjust the postion of the fish on the x - axis
+let y = 250; //adjust the postion of the fish on the y - axis
+
 
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
   background(20)
@@ -22,44 +25,47 @@ let map6 = map(bass, 0, 100, 20, -10)
 
   fill(255, 204, 0)
 
-  // rect(width/1.9 , height/8.5 , 100, 200 )
-  // rect(width/2.5 , height/8.5 , 100, 200 )
-
-  // ellipse(width/2 , height/1.8 , map1, map2)
 
 
-
-  if (bass > 70){
-    ellipse(width/9, height/1.2 + map3, 30, 30)
-
-  } else {
-    ellipse(width/9, height/1.2 , 30, 30)
-  }
+  fishnotevil(x, y, vocal)
 
 
-  if (bass > 70){
-    ellipse(width/15, height/1.2 + map4, 30, 30)
-
-  } else {
-    ellipse(width/15, height/1.2 , 30, 30)
-  }
-
-  if (bass > 70){
-    ellipse(width/1.5, height/1.255 + map5, 100, 100)
-
-  } else {
-    ellipse(width/1.5, height/1.255 , 100, 100)
-
-  }
 
 
-  if (bass > 70){
-    ellipse(width/3, height/1.75 + map6, 500, 500)
 
-  } else {
-    ellipse(width/3, height/1.75 , 500, 500)
 
-  }
+
+  // if (bass > 70){
+  //   ellipse(width/9, height/1.2 + map3, 30, 30)
+
+  // } else {
+  //   ellipse(width/9, height/1.2 , 30, 30)
+  // }
+
+
+  // if (bass > 70){
+  //   ellipse(width/15, height/1.2 + map4, 30, 30)
+
+  // } else {
+  //   ellipse(width/15, height/1.2 , 30, 30)
+  // }
+
+  // if (bass > 70){
+  //   ellipse(width/1.5, height/1.255 + map5, 100, 100)
+
+  // } else {
+  //   ellipse(width/1.5, height/1.255 , 100, 100)
+
+  // }
+
+
+  // if (bass > 70){
+  //   ellipse(width/3, height/1.75 + map6, 500, 500)
+
+  // } else {
+  //   ellipse(width/3, height/1.75 , 500, 500)
+
+  // }
 }
 
 
@@ -74,7 +80,11 @@ let map6 = map(bass, 0, 100, 20, -10)
 
 
 
-function fishnotevil(x, y, face_be_happy, fin) { //the totally definitely not evil fish
+function fishnotevil(x, y, vocal) { //the totally definitely not evil fish
+
+  rotate(270)
+
+  scale(2)
 
 
 
@@ -92,7 +102,7 @@ function fishnotevil(x, y, face_be_happy, fin) { //the totally definitely not ev
   triangle(x - 50, y + .5, x - 44, y - 6, x - 42, y + .5); //read top body fin
 
   fill(0); //black fill for the eye
-  circle(x + 60, y + 5, 5); //eye
+  circle(x + 50, y + 5, 5); //eye
 
   fill(54, 79, 110); //reseting the fill to the blue
 
@@ -102,19 +112,94 @@ function fishnotevil(x, y, face_be_happy, fin) { //the totally definitely not ev
 
   arc(x, y + 13, 98, 10, 0, 180); //body line
 
-  if (face_be_happy >= 1) {
-    arc(x + 60, y + 13, 10, 10, 100, 170); //happy mouth
-  } else {
-    arc(x + 63, y + 13, 10, 5, 180, 350); //sad mouth
+
+
+
+
+  fill(229, 216, 189) //white
+
+  stroke(54, 79, 110) //blue
+
+  // fill(229, 216, 189) // use for setting up fish
+  // stroke(229, 216, 189)
+
+  if (vocal > 30 && vocal < 50){
+
+  fill(54, 79, 110) //reseting the fill to the blue
+
+  triangle(x + 58, y + 2, x + 65, y + 2, x + 75, y - 0)  //top of mouth
+
+  triangle(x + 60, y + 15, x + 63, y + 15, x + 72, y + 10 ) //bottom of mouth
+
+
+  fill(229, 216, 189) //white
+
+   stroke(54, 79, 110) //blue
+
+  arc(x + 74, y + 4, 15, 12, 100, 170);  //mouth 1
   }
+  else{
 
+    arc(x + 73, y + 5, 15, 5, 100, 170); //happy mouth
 
-
-  if (fin >= 1) { //if statment to make the fin show up or not 
-    strokeWeight(1);
-
-    triangle(x + 50, y + 10, x + 48, y + 15, x + 30, y + 18); //body fin
   }
+  
+  if (vocal >= 50 && vocal < 70){
+
+  fill(54, 79, 110) //reseting the fill to the blue
+
+
+  triangle(x + 58, y + 2, x + 65, y + 2, x + 75, y - 0)  //top of mouth
+
+  triangle(x + 60, y + 15, x + 63, y + 15, x + 72, y + 12 ) //bottom of mouth
+
+  fill(229, 216, 189) //white
+
+   stroke(54, 79, 110) //blue
+
+    arc(x + 74, y + 4, 20, 16, 100, 170); // mouth 2
+  }
+ 
+
+  if (vocal >= 70 && vocal < 75) {
+    fill(54, 79, 110) //reseting the fill to the blue
+
+  triangle(x + 58, y + 2, x + 65, y + 2, x + 75, y )  //top of mouth
+
+  triangle(x + 55, y + 9, x + 59, y + 18, x + 70.5, y + 16.5 ) //bottom of mouth
+
+  fill(229, 216, 189) //white
+
+   stroke(54, 79, 110) //blue
+
+    arc(x + 74, y + 4, 25, 25, 100, 170); // mouth 3
+  }
+    
+  if (vocal >= 75) {
+
+    fill(54, 79, 110) //reseting the fill to the blue
+
+    triangle(x + 58, y + 2, x + 65, y + 2, x + 75, y )  //top of mouth
+  
+    triangle(x + 52, y + 12, x + 50, y + 22, x + 69, y + 21 ) //bottom of mouth
+  
+    fill(229, 216, 189) //white
+  
+     stroke(54, 79, 110) //blue
+    arc(x + 74, y + 4, 35, 35, 100, 170); // mouth 3
+  }
+  
+
+  
+
+
+  fill(54, 79, 110); //reseting the fill to the blue
+
+  stroke(229, 216, 189); //setting th stroke color to a off white
+
+
+  strokeWeight(1);
+  triangle(x + 50, y + 10, x + 48, y + 15, x + 30, y + 18); //body fin
 
 
 
@@ -125,16 +210,14 @@ function fishnotevil(x, y, face_be_happy, fin) { //the totally definitely not ev
   strokeWeight(2); //setting the stroke weight so the brim as some weight to it
   stroke(255, 72, 165); //pink color for the hat 
 
-  arc(x + 57, y - 7, 15, 15, 35, 140); //brim of the hat
+  arc(x + 52, y - 7, 15, 15, 35, 140); //brim of the hat
 
   fill(255, 72, 165); //pink color for the hat 
 
-  triangle(x + 55, y - .5, x + 57, y - 8, x + 59, y - .5); //tringle of the hat
+  triangle(x + 50, y - .5, x + 52, y - 8, x + 54, y - .5); //tringle of the hat
+
 
 }
-
-
-
 
 
 
